72367bc4ff6a6b909e8cb366d68890a8b2b867f4
cksdnde <chandol0705@naver.com>
2023-06-20 13:46:35 +0900

    asd

--- common/views.py
+++ common/views.py
@@ -1,6 +1,3 @@
-from django.shortcuts import render
-
-# Create your views here.
 from django.contrib.auth import authenticate, login
 from django.shortcuts import render, redirect
 from common.forms import UserForm
